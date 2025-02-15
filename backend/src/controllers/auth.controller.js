857f77ccdf9e59c8e4b33db30c1c154b2351853a
Lenny Eugene <mysticanxtr8@gmail.com>
2025-02-15 17:16:43 +0300

    Implements user authentication feature
    Adds token generation on user signup
    Saves user details on successful signup
    Improves error handling in signup controller
    
    Fixes #789

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 
@@ -24,14 +25,22 @@ export const signup = async(req,res) =>{
         })
 
         if(newUser){
-            res.status(201).json({message: "User created successfully"});
+            generateToken(newUser._id,res);
+            await newUser.save();
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic,
+            });
         }
         else{
             res.status(400).json({message: "Failed to create user"});
         }
     }
     catch(error){
-        console.log(error);
+        console.log("Error in signup controller:",error.message);
+        res.status(500).json({message: "Internal server error"});
     }
 }
 
