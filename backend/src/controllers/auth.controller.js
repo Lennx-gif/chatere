f15a748a679431e1205a82ef1fd25042cf760720
Lenny Eugene <mysticanxtr8@gmail.com>
2025-02-15 17:03:24 +0300

    Backend Development

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async(req,res) =>{
+    const{fullName,email,password} = req.body;
+    try{
+        if (password.length < 6){
+            return res.status(400).json({message: "Password must be atleast 6 characters long"});
+        }
+
+        const user = await User.findone({email})
+
+        if(user)return res.status(400).json({message: "Email already exists"});
+
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new user({
+            fullName:fullName,
+            email:email,
+            password:hashedPassword,
+
+        })
+
+        if(newUser){
+            res.status(201).json({message: "User created successfully"});
+        }
+        else{
+            res.status(400).json({message: "Failed to create user"});
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+export const login = (req,res) =>{
+    res.send("login route");
+}
+
+export const logout = (req,res) =>{
+    res.send("logout route");
+}
