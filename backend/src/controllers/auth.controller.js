1db5545d92ac61a11ea4f44e1063916bb2d31600
Lenny Eugene <mysticanxtr8@gmail.com>
2025-02-16 19:42:46 +0300

    Add messaging functionality with Cloudinary integration, user profile update, and authentication checks

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup = async(req,res) =>{
     const{fullName,email,password} = req.body;
@@ -86,4 +87,28 @@ export const logout = (req,res) =>{
     }
 };
 
-export const updateProfile = async(req,res) =>{};
+export const updateProfile = async(req,res) =>{
+    try {
+        const {profilePic} = req.body;
+        const userId= req.user._id
+        if(!profilePic){
+            return res.status(4001).json({message:"Profile Picture is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updateUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url})
+        res.status(200).json(updateUser);
+    } catch (error) {
+        console.log("Error in updating profile").error;
+        res.status(500).json({message:"Internal Server Error"});
+    }
+};
+
+export const checkAuth = (req,res) =>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth Controller".error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from '.routes/message.route.js';
 import {connectDB} from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -10,6 +11,8 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+
 app.use(cookieParser());
 
 app.listen(PORT, () => {
