a4f7a9bc3db35e932bf2c240ba27ba702378fffb
Lenny Eugene <mysticanxtr8@gmail.com>
2025-03-14 23:08:20 +0300

    Add CORS support to backend and update frontend dependencies; implement loader in App component and enhance SignupPage layout

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,10 @@ app.use (express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true,
+}));
 
 app.get("/", (req, res) => {
     res.send("Backend is running successfully!");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,23 @@ import ProfilePage from './pages/ProfilePage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import { useAuthStore } from './atore/useAuthStore.js'
 import { useEffect } from 'react'
+import {Loader} from 'lucide-react'
+import { Navigate } from 'react-router-dom'
 
 const App = () => {
-  const {checkAuth,authUser} = useAuthStore();
+  const {checkAuth,authUser,} = useAuthStore();
   useEffect (()=>{
     checkAuth;
   },[checkAuth]);
 
   console.log(authUser);
+
+  //if(isCheckingAuth && !authUser) return (
+    <div className='flex items-center justify-center h-screen '>
+
+      <Loader className='size-10 animate-spin'></Loader> 
+    </div>
+ // )
 
 
 
@@ -24,11 +33,11 @@ const App = () => {
     <div >
       <Navbar/>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/" element={authUser ? <HomePage />:<Navigate to = "/login"/>} />
+        <Route path="/signup" element={authUser ? <SignUpPage /> :<Navigate to = ''/>} />
+        <Route path="/login" element={authUser ? <LoginPage />:<Navigate to = "/"/>} />
+        <Route path="/settings" element={ <SettingsPage /> } />
+        <Route path="/profile" element={authUser ? <ProfilePage />:<Navigate to = "/login"/>} />
 
       </Routes>
         
